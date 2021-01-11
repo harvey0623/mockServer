14c0578fc6e8189dcecc10ca39a9fc31d4f6d324
harvey <harvey@henghung.com>
2021-01-11 10:45:38 +0800

    add notify message

---
+++ dao/mmrm/notify/index.js
@@ -0,0 +1,24 @@
+const mmrmAxios = require('../../../utility/axios/mmrm.js');
+const wmSign = require('../../../utility/crypto/mmrm.js');
+const isDev = process.env.NODE_ENV === 'dev';
+const access_token = isDev ? process.env.MMRM_ACCESS_TOKEN : process.env.CUSTOM_ACCESS_TOKEN;
+const notifyDao = {
+   async memberNotify(payload) {
+      let signText = wmSign({
+         "member_access_token": access_token,
+         request_parameter: { ...payload },
+         "timestamp": "2019/01/01 10:00:05"
+      });
+      return await mmrmAxios({
+         url: '/notification/member_notification_inbox',
+         method: 'post',
+         data: { sign: signText }
+      }).then(res => {
+         return res.data;
+      }).catch(err => {
+         console.log(err);
+      });
+   },
+}
+
+module.exports = notifyDao;
