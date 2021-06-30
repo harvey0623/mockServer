26fa13d40886b645e2247bc32f3963cf89fe7d6e
harvey <harvey@henghung.com>
2021-06-30 11:54:10 +0800

    update

--- route/caltrate/index.js
+++ route/caltrate/index.js
@@ -19,17 +19,20 @@ router.post('/home_delivery/my_order_detail', async (req, res) => {
 
 router.post('/home_delivery/create_order', async (req, res) => {
    let result = await caltrateDao.createOrder(req.body);
-   res.json(result);
+   let statusCode = result.rcrm.RC === 'C01' ? 200 : 400;
+   res.status(statusCode).json(result);
 });
 
 router.post('/couponSearch', async (req, res) => { //查詢行銷票券
-   let results = await caltrateDao.couponSearch(req.body);
-   res.json(results);
+   let result = await caltrateDao.couponSearch(req.body);
+   let statusCode = result.rcrm.RC === 'C01' ? 200 : 400;
+   res.status(statusCode).json(result);
 });
 
 router.post('/couponRedeem', async (req, res) => { //票券核銷
-   let results = await caltrateDao.redeem(req.body);
-   res.json(results);
+   let result = await caltrateDao.redeem(req.body);
+   let statusCode = result.rcrm.RC === 'C01' ? 200 : 400;
+   res.status(statusCode).json(result);
 });
 
 router.post('/couponTransactionCheck', async (req, res) => { //票券檢查
